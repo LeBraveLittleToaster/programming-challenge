3ecf3e7b652418515ee5698cc13c0d7d24b99951
Pascal Schiessle <pascal.schiessle@web.de>
2025-12-09 22:04:52 +0100

    feat: Added header by name and value at functions to table and row

--- src/main/java/de/exxcellent/challenge/data/Row.java
+++ src/main/java/de/exxcellent/challenge/data/Row.java
@@ -1,11 +1,18 @@
 package de.exxcellent.challenge.data;
 
-import java.util.List;
+import java.util.Optional;
 
 /**
  * Represents one datapoint from a dataset.
  * Currently only supports one value type per row.
- * @param values Array of values, can be of any Type T. Consider {@link de.exxcellent.challenge.preprocessing.ITypeParser} instance for transforming the Row to another datatype
+ * @param values Array of values, can be of any Type T. Consider {@link de.exxcellent.challenge.processing.ITypeParser} instance for transforming the Row to another datatype
  * @param <T> generic to capture possible differences in data type
  */
-public record Row<T> (T[] values){}
+public record Row<T> (T[] values){
+    public Optional<T> getValueAt(int index){
+        if(index < 0 || index >= values.length) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(values[index]);
+    }
+}
--- src/main/java/de/exxcellent/challenge/data/Table.java
+++ src/main/java/de/exxcellent/challenge/data/Table.java
@@ -1,8 +1,11 @@
 package de.exxcellent.challenge.data;
 
 import java.util.Map;
+import java.util.Optional;
 
 /**
+ * Inspired by NumPy from Python
+ *
  * Represents a dataset, read by {@link de.exxcellent.challenge.reader.IFileReader} instance.
  *
  * Rows can be mapped to input format and indexing is ensured to be unique by Map constraints
@@ -10,4 +13,8 @@ import java.util.Map;
  * @param rows Represent single lines of data from the source dataset
  * @param <T> Rows are considered of the same type in the current implementation
  */
-public record Table<T> (Map<String, Integer> header, Map<Integer, Row<T>> rows) {}
+public record Table<T> (Map<String, Integer> header, Map<Integer, Row<T>> rows) {
+    public Optional<Integer> getHeaderIndexForName(String headerName){
+        return Optional.ofNullable(header.get(headerName));
+    }
+}
